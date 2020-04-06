f20a33fdd52494910290722f5185e120094c222a
Gaya Rabello <gayarabello@gmail.com>
2020-04-06 02:35:00 +0100

    Adde title to blog

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,16 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Gaya Experience',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content:
+          process.env.npm_package_description ||
+          'Bring me your challenge so we can grow together'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]

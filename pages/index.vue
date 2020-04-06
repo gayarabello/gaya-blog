14fc89a128c1a502e7c9c2347b7e2aa026dce64e
Gaya Rabello <gayarabello@gmail.com>
2020-04-06 02:01:32 +0100

    Added index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,123 @@
 <template>
   <div>
-    <logo />
-    <h1 class="title">Nuxt.js & Netlify CMS Starter</h1>
-    <h2 class="subtitle">Truly fantastic. Sometimes I astound even myself.</h2>
-    <a href="https://github.com/xdesro/nuxt-netlify-cms-starter">GitHub</a>
-    <a href="http://i.ncredibly.online">Twitter</a>
-    <nuxt-link to="/blog">Blog</nuxt-link>
-    <div class="deploy-button">
-      <a
-        href="https://app.netlify.com/start/deploy?repository=https://github.com/xdesro/nuxt-netlify-cms-starter"
-      >
-        <img src="https://www.netlify.com/img/deploy/button.svg" alt="Deploy to Netlify" />
-      </a>
-    </div>
+    <v-container fluid>
+      <v-row class="content-container my-12">
+        <v-col cols="12" sm="6">
+          <h1>
+            <span style="text-decoration: underline #8713F3;"> Bring</span>
+            me your challenge so we can grow together
+          </h1>
+          <div class="py-6" style="color: $blue;">
+            <nuxt-link class="link py-6" to="/contact"> Hire me ></nuxt-link>
+          </div>
+        </v-col>
+        <v-col cols="12" sm="6" class="text-center">
+          <img src="~/assets/images/profile_pic.png" width="240px" />
+          <p
+            style="font-weight: normal; color: #575757; font-size: 14px; line-height: 150%; letter-spacing: 0.3em; text-transform: uppercase;"
+            class="mt-6"
+          >
+            product designer <br />
+            / ux consultant
+          </p>
+        </v-col>
+      </v-row>
+      <v-row class="no-gutters my-12" style="background-color: #fafafa;">
+        <v-row class="content-container align-start align-md-end py-12">
+          <v-col cols="12" md="6">
+            <h5 class="py-6">career path and values</h5>
+            <h2>The values that brought me here</h2>
+            <p class="py-6">
+              I rely on a continuous improvement mindset that makes me learn
+              everyday and constantly enhance my skillset.
+            </p>
+          </v-col>
+          <v-col cols="12" md="6" class="pb-12">
+            <ul>
+              <li class="py-4">
+                <router-link class="link" to="/contact">
+                  Get to know my journey >
+                </router-link>
+              </li>
+              <li>
+                <router-link class="link" to="/contact">
+                  Get to know my personal projects >
+                </router-link>
+              </li>
+            </ul>
+          </v-col>
+        </v-row>
+      </v-row>
+      <v-row class="content-container">
+        <v-col class="text-center" cols="12">
+          <h5 class="py-6">case studies</h5>
+          <h2>From user research to user interface</h2>
+          <p class="py-6">
+            User research, journeys, taskflows, wireframing and prototyping. See
+            how I manage to validate ideas trought my design process.
+          </p>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-card class="mx-auto" max-width="400" height="430px">
+            <v-img
+              class="white--text align-end"
+              height="220px"
+              src="https://images.unsplash.com/flagged/photo-1573763955714-e7114250fdaa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80"
+            >
+              <!-- <v-card-title> <h4>Ainda estou a aprender</h4></v-card-title> -->
+            </v-img>
+
+            <v-card-subtitle class="pb-0"> #EDUCATIONAL </v-card-subtitle>
+            <v-card-text class="py-0 my-0">
+              <h4 class="py-4">Ainda estou a aprender</h4>
+              <p>
+                Educational Platform to help alphabetize young students.
+              </p>
+            </v-card-text>
+
+            <v-card-actions class="my-0">
+              <v-btn color="blue" text>
+                See more
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-card class="mx-auto" max-width="400" height="430px">
+            <v-img
+              class="white--text align-end"
+              height="220px"
+              src="https://early.betmarkets.io/wp-content/uploads/2019/04/concept_v4.jpg"
+            >
+              <!-- <v-card-title> <h4>Ainda estou a aprender</h4></v-card-title> -->
+            </v-img>
+
+            <v-card-subtitle class="pb-0"> #FINTECH </v-card-subtitle>
+            <v-card-text class="py-0 my-0">
+              <h4 class="py-4">Betmarkets.io</h4>
+              <p>
+                Sports Betting Investments platform.
+              </p>
+            </v-card-text>
+
+            <v-card-actions class="my-0">
+              <v-btn color="blue" text>
+                See case study
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+//import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-    Logo
+    //Logo
   },
   head() {
     return {
@@ -33,17 +129,10 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="scss">
+@import '~/assets/css/main.scss';
 
-.title {
+/* .title {
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
     'Helvetica Neue', Arial, sans-serif;
   display: block;
@@ -59,12 +148,14 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
+} */
 a {
   font-weight: bold;
-  color: #526488;
+  color: $blue;
+  font-size: 18px;
 }
-.deploy-button {
-  margin-top: 1rem;
+
+.link {
+  color: $blue !important;
 }
 </style>

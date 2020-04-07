054b98d9e7db94aa59f0931c0575f71eca269c39
Gaya Rabello <gayarabello@gmail.com>
2020-04-07 23:48:29 +0100

    WIP Styled my blog post page

--- pages/blog/_blog.vue
+++ pages/blog/_blog.vue
@@ -1,7 +1,12 @@
 <template>
-  <article>
-    <h1>{{blogPost.title}}</h1>
-    <div v-html="$md.render(blogPost.body)" />
+  <article class="px-8">
+    <v-row class="content-container my-6">
+      <nuxt-link :to="'/blog'"> back to blog</nuxt-link>
+    </v-row>
+    <v-row class="content-container mt-12">
+      <h1>{{ blogPost.title }}</h1>
+      <div class="pt-12" v-html="$md.render(blogPost.body)" />
+    </v-row>
   </article>
 </template>
 <script>
